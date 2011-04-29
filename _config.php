b3e601bc3971dcc726579a4c8c972ed3253c2287
Julian Seidenberg <julian@silverstripe.com>
2011-04-29 13:23:04 +1200

    ENHANCEMENT: increasing the max execution time to one hour to give the module time to count all the links on a complex site

--- _config.php
+++ _config.php
@@ -2,4 +2,6 @@
 Object::add_extension("SiteTree","LinkCountExtension");
 
 if (class_exists('SphinxSearchable')) Object::add_extension('SEOSitemapPage', 'SphinxSearchable');
+
+ini_set('max_execution_time', 14400);  //4 hours max exec time to give the module time to calculate links for all pages
 ?>
